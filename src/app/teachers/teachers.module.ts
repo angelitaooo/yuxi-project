5c545fe039c6bee6e81a02dba6708e873826d123
Angela Ordonez <angelitaooo@me.com>
2016-12-08 14:10:49 -0500

    creating teachers, courses and welcome modules

---
+++ src/app/teachers/teachers.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TeachersComponent } from './teachers.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TeachersComponent]
+})
+export class TeachersModule { }

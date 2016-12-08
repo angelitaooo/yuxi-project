c030eaa72112738a3d85782f5b3615ae12f9e985
Angela Ordonez <angelitaooo@me.com>
2016-12-08 18:06:11 -0500

    add links to navigation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { SharedComponent } from './shared.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [SharedComponent, NavigationComponent],
   exports: [NavigationComponent]

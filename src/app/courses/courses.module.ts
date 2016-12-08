8fc9d8935f699fd482f09c22e63017c22300c5e2
Angela Ordonez <angelitaooo@me.com>
2016-12-08 14:28:13 -0500

    create teacher-detail, course-detail and navigation components, adding html to them

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesComponent } from './courses.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CoursesComponent]
+  declarations: [CoursesComponent, CourseDetailComponent]
 })
 export class CoursesModule { }

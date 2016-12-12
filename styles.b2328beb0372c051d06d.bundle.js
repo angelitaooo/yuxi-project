6e087a877256f786f04403c1cca852028baef054
Angela Ordonez <angelitaooo@me.com>
2016-12-11 22:03:35 -0500

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{380:function(n,o){},687:function(n,o,c){n.exports=c(380)}},[687]);
+webpackJsonp([1,2],{381:function(n,o){},692:function(n,o,c){n.exports=c(381)}},[692]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
